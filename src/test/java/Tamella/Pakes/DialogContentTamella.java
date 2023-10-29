dd4b2c478fdefd1ce56fe4af493809d1cf7da195
tomal <tomaliyeva9@gmail.com>
2023-10-30 00:33:56 +0400

    test

--- src/test/java/Tamella/Pakes/DialogContentTamella.java
+++ src/test/java/Tamella/Pakes/DialogContentTamella.java
@@ -1,12 +1,82 @@
 package Tamella.Pakes;
 
 import Pages.Parent;
+import Utilities.Driver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class DialogContentTamella extends Parent {
+    public DialogContentTamella() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
 
+    @FindBy(xpath = "//ms-add-button[contains(@tooltip,'ADD')]//button")
+    private WebElement scAddButton;
 
+    @FindBy(xpath = "(//input[@data-placeholder='Name'])[2]")
+    private WebElement scName;
 
+    @FindBy(xpath = "ms-dialog-content input[data-placeholder='Code'] ")
+    private WebElement scCode;
 
+    @FindBy(xpath = "//ms-save-button/button")
+    private WebElement scSaveButton;
+
+    @FindBy(xpath = "//div[contains(text(),'successfully')]")
+    private WebElement scSuccessMessage;
+
+    @FindBy(xpath = "//ms-edit-button\n")
+    private WebElement scEdit;
+
+    @FindBy(xpath = "(//ms-delete-button//button)[1]\n")
+    private WebElement scDelete;
+
+    @FindBy(xpath = "//button[@type='submit']\n")
+    private WebElement scDeleteButton;
+
+    @FindBy(xpath = "//div[contains(text(),'Location successfully deleted')]\n")
+    private WebElement scDeleteMessage;
+
+    @FindBy(xpath = "[data-placeholder='Name']")
+    private WebElement scSearchName;
+
+    @FindBy(xpath = "(//input[@data-placeholder='Code'])[1]/n")
+    private WebElement scSearchCode;
+
+    @FindBy(xpath = "//ms-search-button//button")
+    private WebElement scSearchButton;
+
+    public WebElement getWebElement(String strElement) {
+        switch (strElement) {
+            case "scAddButton":
+                return this.scAddButton;
+            case "scName":
+                return this.scName;
+            case "scCode":
+                return this.scCode;
+            case "scSaveButton":
+                return this.scSaveButton;
+            case "scSuccessMessage":
+                return this.scSuccessMessage;
+            case "scEdit":
+                return this.scEdit;
+            case "scDelete":
+                return this.scDelete;
+            case "scDeleteButton":
+                return this.scDeleteButton;
+            case "scDeleteMessage":
+                return this.scDeleteMessage;
+            case "scSearchName":
+                return this.scSearchName;
+            case "scSearchCode":
+                return this.scSearchCode;
+            case "scSearchButton":
+                return this.scSearchButton;
+
+        }
+
+        return null;
+
+    }
 }
--- src/test/java/Tamella/StepDefinitions/_06_SubjectCategories.java
+++ src/test/java/Tamella/StepDefinitions/_06_SubjectCategories.java
@@ -1,5 +1,6 @@
 package Tamella.StepDefinitions;
 
+import Tamella.Pakes.DialogContentTamella;
 import Tamella.Pakes.LeftNavTamella;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
@@ -7,6 +8,7 @@ import io.cucumber.java.en.When;
 
 public class _06_SubjectCategories {
     LeftNavTamella lnt = new LeftNavTamella();
+    DialogContentTamella dct=new DialogContentTamella();
     @When("Navigate to Location setup")
     public void navigateToLocationSetup() {
         lnt.myClick(lnt.getWebElement("educationbutton"));
@@ -16,29 +18,46 @@ public class _06_SubjectCategories {
 
     @When("Add education location")
     public void addEducationLocation() {
+        dct.myClick(dct.getWebElement("scAddButton"));
+        dct.mySendKeys(dct.getWebElement("scName"),"test");
+        dct.mySendKeys(dct.getWebElement("scCode"),"123");
+        dct.myClick(dct.getWebElement("scSaveButton"));
     }
 
     @Then("Added education successfully")
     public void addedEducationSuccessfully() {
+        dct.verifyContainsText(dct.getWebElement("scSuccessMessage"),"succesfully");
     }
 
     @When("Edit education location")
     public void editEducationLocation() {
+        dct.myClick(dct.getWebElement("scEdit"));
+        dct.getWebElement("scName").clear();
+        dct.mySendKeys(dct.getWebElement("scName"), "city");
+        dct.getWebElement("scCode").clear();
+        dct.mySendKeys(dct.getWebElement("scCode"), "9876");
+        dct.myClick(dct.getWebElement("scSaveButton"));
     }
-
     @Then("Edited educatoin successfully")
     public void editedEducatoinSuccessfully() {
+        dct.verifyContainsText(dct.getWebElement("scSuccessMessage"),"successfully");
     }
 
     @When("Delete education locaation")
     public void deleteEducationLocaation() {
+        dct.myClick(dct.getWebElement("scDelete"));
+        dct.myClick(dct.getWebElement("scDeleteButton"));
     }
 
     @Then("Deleted education successfully")
     public void deletedEducationSuccessfully() {
+        dct.verifyContainsText(dct.getWebElement("scDeleteMessage"),"successfully");
     }
 
     @And("Education Location")
     public void educationLocation() {
+        dct.mySendKeys(dct.getWebElement("scSearchName"),"city");
+        dct.mySendKeys(dct.getWebElement("scSearchCode"),"123");
+        dct.myClick(dct.getWebElement("scSearchButton"));
     }
 }
